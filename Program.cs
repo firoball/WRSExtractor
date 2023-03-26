2ca3e7a1e24f4ae6aec0cf53811e322b187044d7
firoball <unsinnspala@firoball.de>
2023-03-27 00:13:01 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Acknex;
+
+namespace Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: " + typeof(Program).Assembly.GetName().Name + " input.wrs [output folder (default: .\\wrs)]");
+            }
+            else
+            {
+                string current = Directory.GetCurrentDirectory();
+                string outputPath = null;
+                if (args.Length > 1)
+                {
+                    if (Path.IsPathRooted(args[1]))
+                    {
+                        outputPath = Path.GetFullPath(args[1]);
+                    }
+                    else
+                    {
+                        outputPath = Path.Combine(current, Path.GetDirectoryName(args[1]));
+                    }
+                }
+                else
+                {
+                    outputPath = Path.Combine(current, "wrs");
+                }
+
+                string wrsFile = Path.GetFullPath(args[0]);
+                if (!File.Exists(wrsFile))
+                {
+                    Console.WriteLine("ERROR: " + wrsFile + " not found!");
+                }
+                else
+                {
+                    if (!Directory.Exists(outputPath))
+                        Directory.CreateDirectory(outputPath);
+
+                    Console.WriteLine("SOURCE: " + wrsFile);
+                    Console.WriteLine("TARGET: " + outputPath);
+                    Console.WriteLine("Start Extractor...");
+                    WRSExtractor.ExtractWRS(wrsFile, outputPath);
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("WRSExtractor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WRSExtractor")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("908ba68d-dca5-44b3-8533-f3809cb118f8")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
