75d3ea154d3819dc79e32e542fdef757c0f8e1da
firoball <unsinnspala@firoball.de>
2025-01-28 00:37:54 +0100

    - remove dependencies on now private external repo - add relevant files directly to this repo

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # WRSExtractor
 Simple command line extractor for Acknex3 WRS files based on the work of [rickomax](https://github.com/rickomax/Acknex).
 This tool is used to unpack all files inside a `.wrs` resource file which was shipped with many Acknex3 games.
+Relevant files were moved into this repo directly, since the original repo went private. Usage has been granted by rickomax.
 
 **Not** compatible with newer Acknex engine versions.
 
@@ -14,3 +15,8 @@ Open _WRSExtractor.sln_ in Visual Studio and build it.
 
 - Input file may include relative or absolute path
 - Output folder defaults to `.\wrs`
+
+
+## Credits
+GameRes.Compression: morkt
+util: rickomax
---
+++ util/PathUtils.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System;
+
+namespace Acknex
+{
+    public static class PathUtils
+    {
+        public static void Remove<T>(ref T[] arr, T item)
+        {
+            var index = Array.IndexOf(arr, item);
+            if (index < 0)
+            {
+                return;
+            }
+            for (var a = index; a < arr.Length - 1; a++)
+            {
+                arr[a] = arr[a + 1];
+            }
+            Array.Resize(ref arr, arr.Length - 1);
+        }
+
+        public static string SanitizePath(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return null;
+            }
+            var invalids = Path.GetInvalidFileNameChars();
+            Remove(ref invalids, Path.AltDirectorySeparatorChar);
+            Remove(ref invalids, Path.DirectorySeparatorChar);
+            Remove(ref invalids, Path.PathSeparator);
+            Remove(ref invalids, Path.VolumeSeparatorChar);
+            var split = path.Split(invalids, StringSplitOptions.RemoveEmptyEntries);
+            path = string.Join("_", split);
+            path = path.Replace('\\', '/');
+            return path;
+        }
+
+        public static string GetFileExtension(string path, bool includeDot = true)
+        {
+            path = SanitizePath(path);
+            var lastDot = path.LastIndexOf('.');
+            if (lastDot < 0)
+            {
+                return null;
+            }
+
+            return path.Substring(includeDot ? lastDot : lastDot + 1).ToLowerInvariant();
+        }
+
+        public static string GetFileDirectory(string filename)
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                return null;
+            }
+
+            filename = SanitizePath(filename);
+            var indexOfSlash = filename.LastIndexOf("/");
+            if (indexOfSlash >= 0)
+            {
+                return filename.Substring(0, indexOfSlash);
+            }
+
+            return null;
+        }
+    }
+}
